017e43f5d9401786910eaa738406b24d96a219f6
Andrew <arolson56@gmail.com>
2019-10-30 11:54:39 -0700

    update heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,14 +118,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.environ['DATABASE_URL'],
-#     }
-# }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
